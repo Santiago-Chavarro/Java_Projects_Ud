9af32d4d5103eb0d78f41dc459653c87b282e663
Santiago-Chavarro <70897094+Santiago-Chavarro@users.noreply.github.com>
2023-08-23 09:44:11 -0500

    Added the tree example

---
+++ First_Programs/Tree.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tree;
+
+import static javax.management.Query.or;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Tree {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Hey there, Im Santiago and Im learning to code in Java");
+       System.out.println("Im going to plant a tree today");
+       System.out.println("  ***   ");
+       System.out.println(" *****   ");
+       System.out.println("*******  ");
+       System.out.println("*******");
+       System.out.println("*******  ");
+       System.out.println(" *****  ");
+       System.out.println("  ***  ");
+       System.out.println("  ***  ");
+       System.out.println("  ***  ");
+       System.out.println("  ***  ");
+    }
+    
+}
