f7ca57891fb80a2581c89d7bd5a05a44fa8ce4b8
Santiago-Chavarro <70897094+Santiago-Chavarro@users.noreply.github.com>
2023-08-23 09:50:56 -0500

    Added the example additionSubstractionExample

---
+++ First_Programs/AdditionSubstractionExampleJava.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package additionsubstractionexample.java;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class AdditionSubstractionExampleJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int monaguilloInZoo = 3;
+        int giraffesInZoo = 4;
+        int animalsInZoo = monaguilloInZoo + giraffesInZoo;
+    
+        System.out.println(animalsInZoo); 
+
+        int numMonaguillosAfterTrade = monaguilloInZoo - 2;
+
+        System.out.println(numMonaguillosAfterTrade);
+
+    }
+    
+}
