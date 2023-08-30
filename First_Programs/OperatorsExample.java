cc8f8360765ae65709fa986b6bf5c03aba66fe3b
Santiago-Chavarro <70897094+Santiago-Chavarro@users.noreply.github.com>
2023-08-29 21:56:54 -0500

    Add files via upload

---
+++ First_Programs/OperatorsExample.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operatorsexample;
+
+/**
+ *
+ * @author Santiago
+ */
+public class OperatorsExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numCookies = 17;
+        numCookies -= 3;
+        System.out.println(numCookies);
+        numCookies /= 2;
+        System.out.println(numCookies);
+    }
+    
+}
