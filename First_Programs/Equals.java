21baeb5a6acc0af9f698214b4b299b1199974db5
Santiago-Chavarro <70897094+Santiago-Chavarro@users.noreply.github.com>
2023-08-29 22:15:15 -0500

    Add files via upload

---
+++ First_Programs/Equals.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package equals;
+
+/**
+ *
+ * @author Santiago
+ */
+public class Equals {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String line1 = "Nah nah nah nah nah nah nah nah nah yeah";
+        String line2 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
+        String line3 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
+        System.out.println(line1.equals(line2)); //Equals is used to compare strings in a boolean variable
+        System.out.println(line2.equals(line3));
+    }
+    
+}
