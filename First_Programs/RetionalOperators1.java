6fbc62466d49c2cfe16ff022c247e959944899b3
Santiago-Chavarro <70897094+Santiago-Chavarro@users.noreply.github.com>
2023-08-30 08:50:30 -0500

    Add files via upload

---
+++ First_Programs/RetionalOperators1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package retionaloperators1;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class RetionalOperators1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int creaditsEarned = 13;
+        int creditsOfSeminar = 8;
+        int creditsToGraduated = 180;
+        int afterSeminar = creaditsEarned + creditsOfSeminar;
+        System.out.println(creaditsEarned > creditsToGraduated);
+        System.out.println(creditsToGraduated < afterSeminar);
+    }
+    
+}
