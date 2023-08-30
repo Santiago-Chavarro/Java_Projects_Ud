f57f87a6cfcbd94858e39554459b25c4d355c709
Santiago-Chavarro <70897094+Santiago-Chavarro@users.noreply.github.com>
2023-08-30 09:26:55 -0500

    Add files via upload

---
+++ First_Programs/Variables.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class Variables {
+    
+    public static void main(String[] args) {
+        String name = "Santiago Chavarro";
+        int yearCreated = 2007;
+        System.out.println(name);
+        System.out.println(yearCreated);
+    }
+    
+}
