f19b684bd80564b6821fc116f7fd1a62869cb23c
Santiago-Chavarro <70897094+Santiago-Chavarro@users.noreply.github.com>
2023-08-30 08:38:12 -0500

    Add files via upload

---
+++ First_Programs/OrderOperations.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package orderoperations;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class OrderOperations {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int oneExpression = 5 % 2 - (4 * 2 - 1);
+        int twoExpression = (3 + (2 * 2 -5)) + 6 - 5;
+        int threeExpression = 5 * 4 % 3 - 2 + 1;
+        System.out.println(oneExpression);
+        System.out.println(twoExpression);
+        System.out.println(threeExpression);
+    }
+    
+}
