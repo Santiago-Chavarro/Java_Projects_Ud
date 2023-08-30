46b20eb397947f088f870d38896aedcf63e1dbab
Santiago-Chavarro <70897094+Santiago-Chavarro@users.noreply.github.com>
2023-08-29 22:24:52 -0500

    Add files via upload

---
+++ First_Programs/Module.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package module;
+
+/**
+ *
+ * @author Santiago
+ */
+public class Module {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int students = 28;
+        int exit = students % 5;
+        System.out.println(exit + " students left the class");
+    }
+    
+}
