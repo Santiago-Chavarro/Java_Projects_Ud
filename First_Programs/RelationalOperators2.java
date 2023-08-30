bba881e96d538e91fba1c67cdb9be7bda7e1e50b
Santiago-Chavarro <70897094+Santiago-Chavarro@users.noreply.github.com>
2023-08-30 09:01:21 -0500

    Add files via upload

---
+++ First_Programs/RelationalOperators2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package relationaloperators2;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class RelationalOperators2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int songsA = 9;
+        int songsB = 41;
+        int albumLength1 = 41;
+        int albumLength2 = 53;
+        boolean sameNumberSongs = songsA == songsB;
+        boolean diferenlength = albumLength1 != albumLength1;
+    }
+    
+}
