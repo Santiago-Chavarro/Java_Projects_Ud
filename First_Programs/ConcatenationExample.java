8afe379b0a395a4d22f3dccc67d06dc0ef579ef4
Santiago-Chavarro <70897094+Santiago-Chavarro@users.noreply.github.com>
2023-08-29 22:04:15 -0500

    Add files via upload

---
+++ First_Programs/ConcatenationExample.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package concatenationexample;
+
+/**
+ *
+ * @author Santiago
+ */
+public class ConcatenationExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int animals = 12;
+        String species = "zebra";
+        String zooDescrip = "Our zoo has " + animals + " " + species + "s!";
+        System.out.println(zooDescrip);
+    }
+    
+}
