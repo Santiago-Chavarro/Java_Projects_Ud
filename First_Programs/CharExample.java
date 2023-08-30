6c9c37b41afb19bd37fe03fc3094ed61b0b857b3
Santiago-Chavarro <70897094+Santiago-Chavarro@users.noreply.github.com>
2023-08-29 19:52:11 -0500

    Add files via upload

---
+++ First_Programs/CharExample.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.charexample;
+
+/**
+ *
+ * @author Santiago
+ */
+public class CharExample {
+
+    public static void main(String[] args) {
+         char expectedGrade = 'C';
+         System.out.println(expectedGrade);
+    }
+}
