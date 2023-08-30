86000a6a441f9e3c317e27028139ced02151af6c
Santiago-Chavarro <70897094+Santiago-Chavarro@users.noreply.github.com>
2023-08-29 22:20:29 -0500

    Add files via upload

---
+++ First_Programs/Keywords.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package keywords;
+
+/**
+ *
+ * @author Santiago
+ */
+public class Keywords {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        final double pi = 3.14; // final is used to prevent the change in value of the variable
+        System.out.println(pi);
+        /*pi = 2*pi; 
+        this code cause a error
+        */
+    }
+    
+}
