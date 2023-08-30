44f9f226453ab2eb953bfaa6822184a966bd79d8
Santiago-Chavarro <70897094+Santiago-Chavarro@users.noreply.github.com>
2023-08-30 09:22:50 -0500

    Add files via upload

---
+++ First_Programs/StringEx.java
@@ -0,0 +1,10 @@
+package stringex;
+
+public class StringEx {
+    
+    public static void main(String[] args) {
+        String openingLyrics = "Yesterday, all my troubles";
+        System.out.println(openingLyrics);
+    }
+    
+}
