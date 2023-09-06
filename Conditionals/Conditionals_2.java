8b795d0e9ecd0870f804e8c5173874366c8748ea
Santiago-Chavarro <70897094+Santiago-Chavarro@users.noreply.github.com>
2023-09-06 09:50:09 -0500

    Add files via upload

---
+++ Conditionals/Conditionals_2.java
@@ -0,0 +1,16 @@
+package conditionals_2;
+
+public class Conditionals_2 {
+
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        
+        if(x + y < 10){
+            System.out.println("x + y is less than 10");
+        }else{
+            System.out.println("x + y is greater than 20");
+        }
+    }
+    
+}
