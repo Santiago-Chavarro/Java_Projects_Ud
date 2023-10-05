28b19b299adec86c8ef16cb1296c9b8d5cc41b59
Loaxert <70897094+Santiago-Chavarro@users.noreply.github.com>
2023-10-05 09:09:26 -0500

    Add files via upload

---
+++ SecondCut/JOptionPane/Welcome.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+public class Welcome {
+    public static void main(String[] args) throws Exception {
+        JOptionPane.showMessageDialog(null, "Welcome\nto\nthe java world");
+    }
+}
