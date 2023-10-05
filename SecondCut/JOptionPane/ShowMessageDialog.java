9c18b834732fd991974df4e73f870feb80037ef3
Loaxert <70897094+Santiago-Chavarro@users.noreply.github.com>
2023-10-05 09:13:22 -0500

    Add files via upload

---
+++ SecondCut/JOptionPane/ShowMessageDialog.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+public class ShowMessageDialog {
+    public static void main(String[] args) throws Exception {
+        JOptionPane.showMessageDialog(null, "Operacion realizada correctamente");
+    }
+}
